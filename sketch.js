0bd5f89993c1784bcbc8b7786c2fdd370c53c591
unknown <angyg4lvez@gmail.com>
2026-06-16 13:12:13 -0600

    mejorado

--- sketch.js
+++ sketch.js
@@ -5,53 +5,48 @@ let options = { maxFaces: 1, refineLandmarks: false, flipHorizontal: false };
 let triangles;
 
 function preload() {
+  // Load the faceMesh model
   faceMesh = ml5.faceMesh(options);
 }
 
 function setup() {
   createCanvas(640, 480);
+  // Create the webcam video and hide it
   video = createCapture(VIDEO);
   video.size(640, 480);
   video.hide();
+  // Load the triangle indices for drawing the mesh
   triangles = faceMesh.getTriangles();
+  // Start detecting faces from the webcam video
   faceMesh.detectStart(video, gotFaces);
 }
 
 function draw() {
+  // Draw the webcam video
   image(video, 0, 0, width, height);
 
+  // Draw all the triangles
   for (let i = 0; i < faces.length; i++) {
     let face = faces[i];
-    let kps = face.keypoints;
-
-   
-    let noseX = kps[1].x;
-
     for (let j = 0; j < triangles.length; j++) {
-      let iA = triangles[j][0];
-      let iB = triangles[j][1];
-      let iC = triangles[j][2];
+      let indices = triangles[j];
+      let pointAIndex = indices[0];
+      let pointBIndex = indices[1];
+      let pointCIndex = indices[2];
+      let pointA = face.keypoints[pointAIndex];
+      let pointB = face.keypoints[pointBIndex];
+      let pointC = face.keypoints[pointCIndex];
 
-      let pA = kps[iA];
-      let pB = kps[iB];
-      let pC = kps[iC];
-
-     
-      if (pA.x < noseX && pB.x < noseX && pC.x < noseX) {
-        noFill();
-        stroke(0, 0, 255);
-        strokeWeight(1);
-        triangle(pA.x, pA.y, pB.x, pB.y, pC.x, pC.y);
-      }
+      noFill();
+      stroke(0, 0, 255);
+      strokeWeight(1);
+      triangle(pointA.x, pointA.y, pointB.x, pointB.y, pointC.x, pointC.y);
     }
   }
 }
 
+// Callback function for when faceMesh outputs data
 function gotFaces(results) {
+  // Save the output to the faces variable
   faces = results;
-  if (faces.length > 0) {
-	console.log(faces[0].keypoints);
-  faceMesh.detect(gotFaces);}
-  
-
 }
